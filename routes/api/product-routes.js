79933d36e577f77d62d9c71b9e5bef50adbab1d8
Khargol <josephklein3@gmail.com>
2020-08-28 15:57:04 -0400

    tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const results = await Product.findAll({
+      include: [{model: Category}, {model: Tag}]
+    });
+    res.json(results);
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const results = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{model: Category}, {model: Tag}]
+    });
+    res.json(results);
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -97,7 +118,7 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id
       }
     });
-    res.status(200).statusMessage('Product deleted');
+    res.json(results);
   }catch(err){
     console.log(err);
     res.status(500).json(err);
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,84 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try{
+    const results = await Tag.findAll({
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
+    res.json(results);
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const results = await Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
+    res.json(results);
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try{
+    const results = await Tag.create(req.body);
+    res.json(results);
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const results = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(results);
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const results = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(results);
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
